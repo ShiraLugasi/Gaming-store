d152c2227e22a74b86112e7629d90efd7f0082e1
Shira <shiralogasi1@gmail.com>
2024-02-17 16:08:08 +0200

    Committing changes to Dockerfile, init.sql, and store

--- DB/init.sql
+++ DB/init.sql
@@ -1,10 +1,9 @@
 -- Create database
 CREATE DATABASE my_database;
 
--- Switch to the new database
 \c my_database;
 
--- Create table
+
 CREATE TABLE games (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
@@ -13,7 +12,7 @@ CREATE TABLE games (
     price NUMERIC(10,2)
 );
 
--- Insert initial data
+
 INSERT INTO games (name, platform, edition, price) VALUES 
 ('FIFA 24', 'PC', 'standard', 49.99),
 ('FIFA 24', 'PS4', 'standard', 59.99),
