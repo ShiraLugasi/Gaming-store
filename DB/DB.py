873e667a8a6776eb5ee1aaa6fd53abb84b202948
ShiraLugasi <112568183+ShiraLugasi@users.noreply.github.com>
2024-03-26 21:45:58 +0200

    Delete DB directory

--- DB/DB.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/my_database"
-
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
